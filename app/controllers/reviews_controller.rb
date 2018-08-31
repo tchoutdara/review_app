a706f0b75dc0803eb1529286680e510fdd08a505
tchoutdara <monkeymilitia444@gmail.com>
2018-08-31 09:27:39 -0600

    start review app

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,13 @@
+class ReviewsController < ApplicationController
+  def index
+    @reviews = Review.all
+  end
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def new
+    @review = Review.new
+  end
+end
---
+++ db/migrate/20180830225927_create_reviews.rb
@@ -0,0 +1,11 @@
+class CreateReviews < ActiveRecord::Migration[5.2]
+  def change
+    create_table :reviews do |t|
+      t.string :name
+      t.string :author
+      t.text :review
+
+      t.timestamps
+    end
+  end
+end
